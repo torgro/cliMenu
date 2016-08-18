2e1271a74f4d58e751d358bd135794e418494857
Tore Grøneng <Tore Grneng>
2016-08-18 13:27:35 +0200

    Update manifest due to bug in update-ModuleManifest Created publish.ps1 script

---
+++ publish.ps1
@@ -0,0 +1,40 @@
+﻿[cmdletbinding()]
+Param(
+    [Parameter(Mandatory)]
+    [string]
+    $APIkey
+)
+
+$tags = @(
+    "Menu"
+    , 
+    "Cli"
+    , 
+    "Sub-Menu"
+    , 
+    "Main-Menu"
+    , 
+    "Show-Command"
+    ,
+    "Console"
+    ,
+    "Menus"
+    ,
+    "Main"
+    ,
+    "Sub"
+    ,
+    "User"
+    ,
+    "Menu"
+)
+
+$fileList = Get-ChildItem -Filter .\functions\*.ps1 | where name -NotLike "*Tests*"
+$ExportedFunctions = New-Object System.Collections.ArrayList
+$fileList | foreach {
+    $null = $ExportedFunctions.Add($_.BaseName)
+}
+
+Update-ModuleManifest -Path .\CliMenu.psd1 -Tags $tags -FunctionsToExport $ExportedFunctions
+
+Publish-Module -NuGetApiKey $APIkey -Name .\CliMenu.psd1
